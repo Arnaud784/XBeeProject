49c312f79eed40336fe96d86c6e862b38d5d2fb7
Arnaud <arnaud.lieveaux@gmail.com>
2021-01-22 15:23:35 +0100

    petite mise en forme

--- src/main/java/XBeeProject/XBeeProject/App.java
+++ src/main/java/XBeeProject/XBeeProject/App.java
@@ -13,21 +13,21 @@ public class App
     public static void main( String[] args )
     {
     	try {
-    	XBee xbee = new XBee();	
-    	
-		xbee.open("COM15", 9600);
-    				
-    	while (true) {
-    	    RxResponseIoSample ioSample;
-    	    
-			ioSample = (RxResponseIoSample) xbee.getResponse();
-
-    	    System.out.println("We received a sample from " + ioSample.getSourceAddress());	
-    				
-    	    if (ioSample.containsAnalog()) {
-    	        System.out.println("10-bit temp reading (pin 20) is " + ioSample.getSamples()[0].getAnalog0());
-    	    }
-    	}
+	    	XBee xbee = new XBee();	
+	    	
+			xbee.open("COM15", 9600);
+	    				
+	    	while (true) {
+	    	    RxResponseIoSample ioSample;
+	    	    
+				ioSample = (RxResponseIoSample) xbee.getResponse();
+	
+	    	    System.out.println("We received a sample from " + ioSample.getSourceAddress());	
+	    				
+	    	    if (ioSample.containsAnalog()) {
+	    	        System.out.println("10-bit temp reading (pin 20) is " + ioSample.getSamples()[0].getAnalog0());
+	    	    }
+	    	}
     	} catch (XBeeException e) {
     		
     	}
