33fc595a1aefbf9fe9fd18d66b7e9a8b31ae81ce
Arnaud <arnaud.lieveaux@gmail.com>
2021-01-22 14:45:35 +0100

    Création du projet maven + dépendance xbee

---
+++ src/main/java/XBeeProject/XBeeProject/App.java
@@ -0,0 +1,13 @@
+package XBeeProject.XBeeProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
