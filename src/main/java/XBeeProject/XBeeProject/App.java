00c5849d501810947d590f204ee4453c8e47285f
Arnaud <arnaud.lieveaux@gmail.com>
2021-01-22 15:18:17 +0100

    code pour recevoir des samples

--- src/main/java/XBeeProject/XBeeProject/App.java
+++ src/main/java/XBeeProject/XBeeProject/App.java
@@ -1,5 +1,9 @@
 package XBeeProject.XBeeProject;
 
+import com.rapplogic.xbee.api.XBee;
+import com.rapplogic.xbee.api.XBeeException;
+import com.rapplogic.xbee.api.wpan.RxResponseIoSample;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,24 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+    	XBee xbee = new XBee();	
+    	
+		xbee.open("COM15", 9600);
+    				
+    	while (true) {
+    	    RxResponseIoSample ioSample;
+    	    
+			ioSample = (RxResponseIoSample) xbee.getResponse();
+
+    	    System.out.println("We received a sample from " + ioSample.getSourceAddress());	
+    				
+    	    if (ioSample.containsAnalog()) {
+    	        System.out.println("10-bit temp reading (pin 20) is " + ioSample.getSamples()[0].getAnalog0());
+    	    }
+    	}
+    	} catch (XBeeException e) {
+    		
+    	}
     }
 }
