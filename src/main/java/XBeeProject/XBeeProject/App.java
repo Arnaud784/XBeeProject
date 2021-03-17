882c922a73a40911e9fb9a12cb67253ab65cf5ee
GladysTav <gladys.tavenaux@gmail.com>
2021-03-17 10:35:37 +0100

    c'est mieux

--- src/main/java/XBeeProject/XBeeProject/App.java
+++ src/main/java/XBeeProject/XBeeProject/App.java
@@ -3,6 +3,8 @@ package XBeeProject.XBeeProject;
 import com.rapplogic.xbee.api.XBee;
 import com.rapplogic.xbee.api.XBeeException;
 import com.rapplogic.xbee.api.wpan.RxResponseIoSample;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Hello world!
@@ -10,13 +12,16 @@ import com.rapplogic.xbee.api.wpan.RxResponseIoSample;
  */
 public class App 
 {
+	private final static Logger log = Logger.getLogger(App.class);
+	
     public static void main( String[] args )
     {
+    	PropertyConfigurator.configure("log4j.properties");
     	try {
 	    	XBee xbee = new XBee();	
 	    	
-			xbee.open("COM15", 9600);
-	    				
+			xbee.open("COM7", 9600);
+			System.out.println("Port ouvert !");			
 	    	while (true) {
 	    	    RxResponseIoSample ioSample;
 	    	    
@@ -27,9 +32,11 @@ public class App
 	    	    if (ioSample.containsAnalog()) {
 	    	        System.out.println("10-bit temp reading (pin 20) is " + ioSample.getSamples()[0].getAnalog0());
 	    	    }
-	    	}
+	    	}	
     	} catch (XBeeException e) {
     		
     	}
+    	
+		System.out.println("Fin du try");
     }
 }
